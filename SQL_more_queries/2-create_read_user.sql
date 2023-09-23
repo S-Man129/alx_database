3a3a5696b08fea5fd7239ba8e75a2eaabc6e10fe
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-23 18:13:30 +0100

    2. Read  user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,10 +3,12 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grant SELECT privilege on the hbtn_0d_2 database to the user
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+-- Grant privileges and generate output
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
--- Reload privileges to apply changes
-FLUSH PRIVILEGES;
+-- Display the generated output
+SELECT CONCAT('Grants for user_0d_2@localhost') AS GrantInfo;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
