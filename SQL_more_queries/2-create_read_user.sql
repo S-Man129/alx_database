711df459f9fde868021d27852af1538c395e76a0
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-23 18:09:00 +0100

    2. Read  user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,12 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the hbtn_0d_2 database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+
+-- Reload privileges to apply changes
+FLUSH PRIVILEGES;
