c911468f9eda14a20a71131b0a8adf0749785583
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-23 18:06:30 +0100

    1. Root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,10 @@
 -- Create user 'user_0d_1' on MySQL server
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+
+-- Reload privileges to apply changes
+FLUSH PRIVILEGES;
